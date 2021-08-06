ff31f3ff9fc82bf9d212bb332021eec94e3b8597
DosSantosBrunoo <bruno.dossantos@anima.edu.uy>
2021-08-06 08:53:31 -0300

    api controller

--- api-bank/app/Http/Controllers/ApiController.php
+++ api-bank/app/Http/Controllers/ApiController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            "success" => true,
+            "data" => $result,
+            "message" => $message
+        ];
+        return response()->json($response, 200);
+    }
+
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+        $response = [
+            "success" => false,
+            "error" => $error,
+            "errorMessages" => $errorMessages
+        ];
+        return response()->json($response, $code);
+    }
 }
